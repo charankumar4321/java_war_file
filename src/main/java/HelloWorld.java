c9d4fc23f4a7e83b79d9e813c9aaa2b2ac4e8f17
Charan Kumar <charan.kumar@ecolab.com>
2020-01-31 11:07:55 +0530

    second commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,11 +4,11 @@ import static spark.Spark.get;
 
 public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
-		new HelloWorld().init();
+		new HelloWorld1().init();
 	}
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello", (req, res) -> "Hello World1");
 	}
 }
